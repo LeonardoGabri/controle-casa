13b5b9ed8f8d0381b6a620b5931002abeec04fe1
Leonardo Gabriel <leonardogabriel8@hotmail.com>
2026-01-29 13:31:17 -0300

    ajuste na geração de token

--- src/main/java/org/acme/auth/JwtGenerator.java
+++ src/main/java/org/acme/auth/JwtGenerator.java
@@ -2,6 +2,7 @@ package org.acme.auth;
 
 import io.smallrye.jwt.build.Jwt;
 
+import java.time.Instant;
 import java.util.Set;
 
 public class JwtGenerator {
@@ -9,6 +10,7 @@ public class JwtGenerator {
         return Jwt.issuer("example.com")
                 .upn(username)
                 .groups(roles)
+                .expiresAt(Instant.now().plusSeconds(1200))
                 .sign();
     }
 }
