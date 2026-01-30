11aacee8dfd9ed6038746219e2660e39abfb62ab
Leonardo Gabriel <leonardogabriel8@hotmail.com>
2026-01-30 20:27:02 -0300

    Retirando constraint de valor de despesa e parcela

---
+++ src/main/resources/db/migration/V30__delete_constraint_parcela_despesa.sql
@@ -0,0 +1,14 @@
+ALTER TABLE despesa
+DROP CONSTRAINT despesa_valor_total_check;
+
+ALTER TABLE despesa
+DROP CONSTRAINT despesa_valor_total_ativo_check;
+
+ALTER TABLE parcela
+DROP CONSTRAINT parcela_valor_check;
+
+ALTER TABLE parcela
+DROP CONSTRAINT parcela_valor_check1;
+
+
+
